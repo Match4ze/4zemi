321d7e16ce920f17820640934ed46db04194ba96
mayuNomoto <g18942nm@gm.tsuda.ac.jp>
2021-10-17 19:09:46 +0900

    “”

---
+++ myApp/forms.py
@@ -0,0 +1,112 @@
+from django import forms
+from .models import login
+from .models import UserDetail
+from .models import hobby
+from .models import personal
+from .models import question
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+   password = forms.CharField(widget=forms.PasswordInput(),label="パスワード")
+   class Meta():
+      model = User
+      fields = (
+         'username',
+         'email',
+         'password',
+      )
+      labels = {
+         'username':'ユーザー名*',
+         'email':'メールアドレス*',
+         'password':'パスワード*',
+      }
+
+class AddUserForm(forms.ModelForm):
+   class Meta():
+      model = login
+      fields = (
+         'picture',
+         'last_name',
+         'first_name',
+         'birth',
+         'school_name',
+         'school_major',
+         'school_grade',
+         'sexuality',
+         'insta_ID',
+         'twitter_ID',
+         'SNS_name1',
+         'SNS_ID1',
+         'SNS_name2',
+         'SNS_ID2',
+      )
+      labels = {
+         'picture':'プロフィール画像',
+         'last_name':'名字',
+         'first_name':'名前',
+         'birth':'生年月日*',
+         'school_name':'学校名*',
+         'school_major':'専攻*',
+         'school_grade':'学年*',
+         'sexuality':'性別*',
+         'insta_ID':'InstagramのID',
+         'twitter_ID':'TwitterのID',
+         'SNS_name1':'その他SNSの種類',
+         'SNS_ID1':'その他SNSのID',
+         'SNS_name2':'その他SNSの種類',
+         'SNS_ID2':'その他SNSのID',
+      }
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+
+    
+class UserDetailForm(forms.ModelForm):
+   class Meta():
+      model = UserDetail
+      fields = (
+         'photo1',
+         'photo2',
+         'photo3',
+         'photo4',
+         'photo5',
+         'photo6',
+         'heart',
+         'profile_text',
+      )
+      labels = {
+         'photo1':'画像1',
+         'photo2':'画像2',
+         'photo3':'画像3',
+         'photo4':'画像4',
+         'photo5':'画像5',
+         'photo6':'画像6',
+         'heart':'いいね',
+         'profile_text':'プロフィール',
+      }
+         
+class SelectHobby(forms.ModelForm):
+   class Meta():
+      model = hobby
+
+      fields = (
+         'hobby1',
+         'hobby2',
+         'hobby3',
+      )
+      labels = {
+         'hobby1':'1番好きな趣味*',
+         'hobby2':'2番目に好きな趣味',
+         'hobby3':'3番目に好きな趣味',
+      }
+
+class personalForm(forms.ModelForm):
+   class Meta:
+      model = question
+      fields = (
+         'q1','q2','q3','q4','q5','q6','q7','q8','q9','q10'
+         )
+      
---
+++ myApp/urls.py
@@ -0,0 +1,67 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from django.conf.urls import url
+
+
+
+urlpatterns = [
+    path('', views.new_register, name='new_register'),
+    path('<int:id>/topScreen', views.topScreen, name='topScreen'),
+    path('details_screen', views.details_screen, name='details_screen'),
+     #性格診断
+    path('<int:id>/personal', views.personal_view, name='personal'),
+    path('<int:id>/personal2', views.personal2, name='personal2'),
+    path('<int:id>/select', views.select, name='select'),
+    #フレンド申請
+    path('<int:id>/friend_request', views.friend_request, name='friend_request'),
+    path('<int:id>/friend_req_list', views.friend_req_list, name='friend_req_list'),
+    path('<int:id>/<int:allow_id>/friend_allow', views.friend_allow, name='friend_allow'),
+    #ユーザの詳細情報を表示する処理を呼び出す
+    path('<int:id>', views.showDetail, name='showDetail'),
+    #ユーザの登録フォームを呼び出す
+    path('create', views.showCreateUserForm, name='showCreateForm'),
+    #ユーザ登録完了
+    path('create_completion', views.create_completion, name='create_completion'),
+    #ユーザ情報
+    path('showUsers', views.showUsers, name='showUsers'),
+    #ユーザ登録する処理を呼び出す
+    path('add', views.addUser, name='addUser'),
+    #ログイン
+    path('login_user', views.Login, name='login_user'),
+    #ログアウト
+    path("logout",views.Logout,name="Logout"),
+    #ユーザ情報更新フォーム
+    path("<int:id>/user_update",views.UserUpdate, name='user_update'),
+    #ユーザ情報更新
+    path('<int:id>/updateUser', views.updateUser, name='updateUser'),
+    #マイページ登録画面を呼び出す
+    path('<int:id>/user_adddetail', views.showUserDetail, name='user_adddetail'),
+    #マイページ登録
+    path('<int:id>/add_userdetail', views.addUserDetail, name='add_userdetail'),
+    #マイページ更新画面を呼び出す
+    path("<int:id>/mypage_update",views.MypageUpdate, name='Mypage_update'),
+    #マイページ更新
+    path('<int:id>/updateMypage', views.updateMypage, name='updateMypage'),
+    #マイページ
+    path('<int:id>/Mypage', views.showMypage, name='Mypage'),
+
+    #趣味選択フォームを呼び出す
+    path('<int:id>/show_selectHobby', views.showSelectHobby, name='show_selectHobby'),
+    #趣味選択
+    path('<int:id>/add_selectHobby', views.addSelectHobby, name='add_selectHobby'),
+    #趣味選択を編集するフォームを呼び出す
+    path('<int:id>/selectHobby_update', views.showUpdateSelectHobby, name='selectHobby_update'),
+    #趣味選択の更新
+    path('<int:id>/updateSelectHobby', views.updateSelectHobby, name='updateSelectHobby'),    
+
+    #退会確認
+    path('<int:id>/userCheckDelete', views.UserCheckDelete, name='userCheckDelete'),
+    #退会
+    path('<int:id>/userDelete', views.UserDelete, name='userDelete'),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
